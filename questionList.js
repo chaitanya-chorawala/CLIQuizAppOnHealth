e0572e4b68a7129925d9c40ec79836bbed65d347
Chaitanya Chorawala <82606108+chaitanya-chorawala@users.noreply.github.com>
2021-10-13 16:27:33 +0000

    Initial commit

---
+++ questionList.js
@@ -0,0 +1,29 @@
+const questions = [
+  {
+    question : "How much exercise do experts recommend a day?",
+    answer : "60min",
+    options: ["60min","5min","0min","10min"]
+  },
+  {
+    question : "Which is least likely to lower your blood pressure?",
+    answer : "whole-grain bread",
+    options: ["low-fat yogurt","whole-grain bread","spinach","broccoli"]
+  },
+  {
+    question : "Which is least likely to cause food poisoning?",
+    answer : "mayonnaise",
+    options: ["chicken","salad","mayonnaise","beef"]
+  },
+  {
+    question : "When we eat food, it gives us energy to live and grow. This fruit is dried in the sun to make raisins,grows on a vine, and is a healthy snack. Which fruit is it?",
+    answer : "grape",
+    options: ["grape","orange","apple","watermelon"]
+  },
+  {
+    question : "On a hot day it is important to drink a lot of _______.",
+    answer : "water",
+    options: ["soda","sports drinks","water","fruit juice"]
+  },
+]
+
+module.exports = questions;
